454614d4bde9b282d16c33c63ac8b2c1063eb5d0
citst131 <katiafernando40@gmail.com>
2026-03-26 22:13:40 -0300

    Refactor age and gender analysis in exercicios.py

--- exercicios python e c/exercicios.py/ex056.py
+++ exercicios python e c/exercicios.py/ex056.py
@@ -1,42 +1,45 @@
-idade_total = 0
+
 total_pessoas = 0
-sexo_homem = ['masculino', 'macho', 'homem']
-sexo_mulher = ['feminino', 'femea', 'mulher']
-nome_homemVelho = ''
-idade_homemVelho = 0
-mulheres_menosDe20Anos = 0
-total_homem = 0
-
+total_idade = 0
+idade_homem_velho = 0
+mulheres_menos_20_anos = 0
+total_homens = 0
+total_mulheres = 0
+nome_homem_velho = ''
 for i in range(4):
+    print(f'-----{i + 1}ª pessoa-----')
+    nome = input("Nome: ").title()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo[M/F]: ")).lower().replace(" ", "")
 
-    nome = input('Digite seu nome: ')
-    idade = int(input('Digite sua idade: '))
-    sexo = input('Digite o seu sexo: ').lower().replace(' ', '')
-
-    while sexo not in sexo_mulher and sexo not in sexo_homem:
-        print('Sexo invalido! Apenas (mulher, femea e feminino) para mulheres e (macho, masculino e homem) para os homens!')
-        sexo = input('Digite o seu sexo: ').lower().replace(' ', '')
+    while sexo != 'm' and sexo != 'f':
+        print("Sexo invalido!")
+        sexo = str(input("Sexo[M/F]: ")).lower().strip()
 
     total_pessoas += 1
-    idade_total += idade
+    total_idade += idade
 
-    if sexo in sexo_homem:
-        total_homem += 1
-        if idade > idade_homemVelho:
-            idade_homemVelho = idade
-            nome_homemVelho = nome
-
-    elif sexo in sexo_mulher:
+    if sexo == 'm':
+        total_homens += 1
+        if idade > idade_homem_velho:
+            idade_homem_velho = idade
+            nome_homem_velho = nome
+    else:
+        total_mulheres += 1
         if idade < 20:
-            mulheres_menosDe20Anos += 1
+            mulheres_menos_20_anos += 1
 
-media = idade_total / total_pessoas
 
-print(f'A media de idade do grupo é {media:.0f} anos!')
+media = total_idade / total_pessoas
 
-if total_homem > 0:
-    print(f'O homem mais velho é o {nome_homemVelho} com {idade_homemVelho} anos!')
+print(f"A media de idade do grupo é {media:.1f} anos!")
+
+if total_homens > 0:
+    print(f"O Homem mais velho é o {nome_homem_velho} com {idade_homem_velho} anos!")
 else:
-    print('Não tem homens nesse grupo!')
+    print("Não tem homem no grupo!")
 
-print(f'{mulheres_menosDe20Anos} mulheres tem menos de 20 anos!')
+if total_mulheres > 0:
+    print(f"{mulheres_menos_20_anos} mulher(es) tem menos de 20 anos!")
+else:
+    print("Não tem mulheres no grupo!")
